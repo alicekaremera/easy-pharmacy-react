c4b65b88a4e65833497d7314569f072b5cdc1d9a
Gentille Uwera <gentox89@gmail.com>
2022-01-25 14:03:42 +0200

    changes on homelayout

---
+++ src/views/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import HomeLayout from "../components/homeLayout";
+
+const Home=()=>{
+
+    return(
+<div className= "home-bg">
+    
+     <HomeLayout>
+    <h1>wertyuiol,kmnbvcxzaqwerty</h1>
+     </HomeLayout> 
+    
+
+
+
+</div>
+    )
+}
+
+export default Home;
