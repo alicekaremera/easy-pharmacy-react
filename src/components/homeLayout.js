740d4f80378ce59c36913b70a0944498e59000e4
alicekaremera <karemeraalice@gmail.com>
2022-01-26 23:51:34 -0800

    footer

--- src/components/homeLayout.js
+++ src/components/homeLayout.js
@@ -1,14 +1,14 @@
 
 import React from "react";
 import "./homeLayout.css";
-  // import Header from "./header";
+   import Header from "./header";
  import Footer from "./footer";
  
 const HomeLayout = ({ children }) => {
   return (
     <div className="home-container" >
         <div className="home-container1">
-       {/* <Header />   */}
+        <Header />  
          {children}
         <Footer /> 
       </div>
