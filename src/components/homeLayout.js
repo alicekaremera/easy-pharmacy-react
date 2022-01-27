2ee93c323001316b5d2b67d3a457c079fb5291e6
lysa27 <lysaadeline27@gmail.com>
2022-01-27 12:24:20 +0200

    footer changes

--- src/components/homeLayout.js
+++ src/components/homeLayout.js
@@ -1,26 +1,23 @@
 import React from "react";
 import { Children } from "react/cjs/react.production.min";
 import "./homeLayout.css";
-// import Header from "./header";
-// import Footer from "./footer";
+  import Header from "./header";
+//  import Footer from "./footer";
 
 
 
 const HomeLayout = ({ children }) => {
   return (
-    <div className="home-container" style={{}}>
-        <Header />  
-      <div style={{ minHeight: "100vh" }}>
     <div className="home-container" >
         <div className="home-container1">
             
-      {/* <Header /> */}
-      {/* <div style={{ minHeight: "100vh",background:"grey" }}> */}
-        {children}
-      {/* </div> */}
+       <Header />   
+       <div style={{ MinHeight:"100vh"}}> 
+         {children} 
+         </div>
 
 
-      {/* <Footer /> */}
+       {/* <Footer />  */}
       </div>
     </div>
 
---
+++ src/views/aboutUs.js
@@ -0,0 +1,9 @@
+import React from "react";
+import HomeLayout from "../components/homeLayout";
+const Home=()=>{
+    return(
+        <HomeLayout>
+            
+        </HomeLayout>
+    )
+}
