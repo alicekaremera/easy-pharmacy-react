78ff21d32d4ff7a756101ec02d5e775b7cde0b05
kagaju <kagajugrace@gmail.com>
2022-02-02 10:36:16 +0200

    pharmacy page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Home from "../views/home";
+import Pharmacy from '../views/pharmacy';
 import {Routes, Route} from "react-router-dom";
 
  const Index=()=>{
 
     return(
         <Routes>
-            <Route exact path ='/home' element={<Home/>}></Route>
+            <Route exact path ='/' element={<Home/>}></Route>
+            <Route element={<Pharmacy/>} path="/pharmacy">
+
+           </Route>
         </Routes>
+
+        
     );
  }
  export default Index;
---
+++ src/views/pharmacy.js
@@ -0,0 +1,122 @@
+import React from "react";
+// import Homelayout from "../components/homelayout";
+import"./pharmacy.css";
+const Pharmacy=()=>{
+    return(
+        // <Homelayout>
+    <div className="pharmacy-container">
+        
+     <h1>Pharmacies List</h1>
+     <div className="list">
+
+<div className="listitem">Pharmacy Name</div>
+
+<div  className="listitem" style={{ marginLeft: "300px"}}>Main Phone</div>
+
+<div  className="listitem" style={{ marginLeft: "400px"}}>Details</div>
+
+</div>
+
+<div className="ourpharmacy">
+
+            <div className="pharmacyname">
+
+            <h2>KIPHARMA Pharmacy</h2>
+
+            <p>Address: Kigali, City market
+
+                 <br/>
+
+                 <h3>KN 302 ST</h3>
+
+                 
+
+            </p>
+
+           
+
+            </div>
+
+            <div className="phone">
+                <h1>0788899929</h1>
+            </div>
+
+            
+            <div className="read">
+                <h1>Read more...</h1>
+            </div>
+
+        </div>
+
+
+
+        <div className="ourpharmacy">
+
+            <div className="pharmacyname">
+
+            <h2>Pharmacy La CONSEIL</h2>
+
+            <p>Address: Kigali, City market
+
+                 <br/>
+
+                 <h3>KN 302 ST</h3>
+
+                 
+
+            </p>
+
+           
+
+            </div>
+
+            <div className="phone">
+                <h1>0788899929</h1>
+            </div>
+
+            
+            <div className="read">
+                <h1>Read more...</h1>
+            </div>
+
+        </div>
+
+
+<div className="ourpharmacy">
+
+<div className="pharmacyname">
+
+<h2>UBUMWE Pharmacy</h2>
+
+<p>Address: Kigali, City market
+
+     <br/>
+
+     <h3>KN 302 ST</h3>
+
+     
+
+</p>
+
+
+
+</div>
+
+<div className="phone">
+    <h1>0788899929</h1>
+</div>
+
+
+<div className="read">
+    <h1>Read more...</h1>
+</div>
+
+</div>
+        
+    </div>
+
+        // </Homelayout>
+
+    );
+}
+export default Pharmacy;
