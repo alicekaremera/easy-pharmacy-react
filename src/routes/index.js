fa53c994ff6c7a3eec149da7a64d044d57562762
lysa27 <lysaadeline27@gmail.com>
2022-02-04 11:33:56 +0200

    changes on payment

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import Home from "../views/home";
 import SignUp from "../views/signup";
 import {Routes, Route} from "react-router-dom";
 import AboutUs from "../views/aboutus";
+import Payment from "../views/paymentLogin";
 
 
  const Index=()=>{
@@ -12,6 +13,7 @@ import AboutUs from "../views/aboutus";
             <Route exact path ='/home' element={<Home/>}></Route>
             <Route exact path ='/signup' element={<SignUp/>}></Route >
             <Route exact path ='/about' element={<AboutUs/>}></Route>
+            <Route exact path ='/payment' element={<Payment/>}></Route>
         </Routes>
     );
  }
